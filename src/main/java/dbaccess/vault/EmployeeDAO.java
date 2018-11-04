298bacdaed319393113df7cbb3100382e3e6f63c
Ana Laura Salazar <analaurasalazar@MacBook-Air-de-Ana.local>
2018-11-04 10:22:13 -0300

    Demo1

--- src/main/java/dbaccess/vault/EmployeeDAO.java
+++ src/main/java/dbaccess/vault/EmployeeDAO.java
@@ -53,7 +53,7 @@ public class EmployeeDAO extends GenericDAO {
 
 		 //String query = "select new model.Employee(c.firstName,c.lastName,c.email,c.phoneNumber,c.hireDate,c.job,c.salary,c.commissionPct,c.department,c.jobHistories) from EMPLOYEES c join c.job j where j.id = :id";
 		String query = "from EMPLOYEES"; 
-		Query a = this.getEntityManager().createQuery(query).setParameter("id", id);
+		Query a = this.getEntityManager().createQuery(query);//.setParameter("id", id);
 		 List<Employee>  employeeList = (List<Employee>) a.getResultList();
 		 System.out.println("Antes de entrar al for: " );		 
 		 for(Employee employee : employeeList) {
