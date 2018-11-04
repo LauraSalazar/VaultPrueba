184a0b04fe5f88ec0c6b29dc4e885e36958a803e
Ana Laura Salazar <analaurasalazar@MacBook-Air-de-Ana.local>
2018-11-04 19:17:13 -0300

    Demo1

--- src/main/java/model/EmployeePersist.java
+++ src/main/java/model/EmployeePersist.java
@@ -1,10 +1,11 @@
 package model;
 
 import java.time.LocalDate;
+import java.util.List;
 
 public class EmployeePersist extends Employee{
 	public EmployeePersist(String firstName, String lastName, String email, String phoneNumber, LocalDate hireDate, Job job,
-			Double salary, Double commissionPct, Department department) {
+			Double salary, Double commissionPct, Department department,List<JobHistory> jobHistory) {
 		super();
 		this.firstName = firstName;
 		this.lastName = lastName;
