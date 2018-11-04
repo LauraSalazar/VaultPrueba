0218ba074c400e98636c49fd9f02dc326f9c5aa2
Ana Laura Salazar <analaurasalazar@MacBook-Air-de-Ana.local>
2018-11-04 05:54:57 -0300

    Demo1

--- src/main/java/dbaccess/vault/EmployeeDAO.java
+++ src/main/java/dbaccess/vault/EmployeeDAO.java
@@ -52,7 +52,7 @@ public class EmployeeDAO extends GenericDAO {
 	@Transactional
 	public List<Employee> getEmpleadosByJobId(Integer id){
 
-		 String query = "select new model.Employee(c.firstName,c.lastName,c.email,c.phoneNumber,c.hireDate,c.job,c.salary,c.commissionPct,c.manager,c.department) from EMPLOYEES ";
+		 String query = "select new model.Employee(c.firstName,c.lastName,c.email,c.phoneNumber,c.hireDate,c.job,c.salary,c.commissionPct,c.manager,c.department) from EMPLOYEES c";
 		 Query a = this.getEntityManager().createQuery(query);//.setParameter("id", id);
 		 List<Employee>  employeeList = (List<Employee>) a.getResultList();
 		 System.out.println("Antes de entrar al for: " );		 
