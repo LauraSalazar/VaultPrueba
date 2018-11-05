f54c7a7275513662c1339beae07b85f7527b004c
Ana Laura Salazar <analaurasalazar@MacBook-Air-de-Ana.local>
2018-11-04 21:49:45 -0300

    Demo1

--- src/main/java/model/JobHistory.java
+++ src/main/java/model/JobHistory.java
@@ -10,8 +10,6 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
-
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.google.gson.annotations.Expose;
 
 @Entity
