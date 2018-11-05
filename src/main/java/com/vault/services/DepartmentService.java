4b88e1856370b4ea80697c376edc6b344a36d9de
Ana Laura Salazar <analaurasalazar@MacBook-Air-de-Ana.local>
2018-11-05 02:02:13 -0300

    Demo1

--- src/main/java/com/vault/services/DepartmentService.java
+++ src/main/java/com/vault/services/DepartmentService.java
@@ -33,7 +33,7 @@ public class DepartmentService {
 					Department d = new Department();
 					d.setDepartmentName(departmentName);
 					d.setManager(null);
-					d.setLocation(null);
+					d.setLocation(locDAO.findById(locationId));
 					deptDAO.create(d);
 					message = "Se agrego el department porque estamos antes del 15 y el promedio es mayor que 1000";
 				} else {
